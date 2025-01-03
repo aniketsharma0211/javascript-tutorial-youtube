46458f5cf8d8dc5f08a26334930ecf3f676a4017
aniketsharma0211 <52794355+aniketsharma0211@users.noreply.github.com>
2025-01-03 04:27:51 +0000

    conversion operations

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all js code below as new version in entire file. this is the DEFAULT MODE.
+
+//alert("Test"); we are using nodejs and not browser.
+
+//most places we avoid SEMI_COLON. next line can act as delimeter like semi colon.
+
+let name = "test"
+let age = 18
+let isLoggedIn = true
+
+//number => 2^52 
+//bigint
+//string both single and double quote
+//boolean
+// null => standalone  value. means empty.
+// undefined => when value is not defined. variable declared but value not assigned
+// symbol => uniqueness
+
+
+
+// object
+
+
+console.log(typeof "");//returns string
